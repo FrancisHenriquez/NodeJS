4edd694f447edfe0f7b4d35b5cc08adcd27dd690
FrancisHenriquez <francishenriquez07@gmail.com>
2023-01-19 11:42:03 -0400

    request and response

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const server = http.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers);
     res.setHeader('Conten-type', 'text/html');
     res.write('<html> <head><title> My First Pagoe </title></head> <body> <h1>Dont be sorry father be better</h1></body></html>')
+    res.end();
 });
 
 server.listen(3000);
