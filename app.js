f03745e0776e7f4a057e6129e757d452b8e92d6b
FrancisHenriquez <francishenriquez07@gmail.com>
2023-01-25 10:15:04 -0400

    middleware control

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-const http = require ('http');
 
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
-});
-
-app.use((req, res, next) => {
+app.use( '/add-product', (req, res, next) => {
     console.log('In the middleware! of the middleware');
-    next();
+    res.send('<h1> God of War script</h1>');
 });
 
-const server = http.createServer( app );
-
-server.listen(3000);
+app.use( '/', (req, res, next) => {
+    console.log('In the middleware! of the middleware');
+    res.send('<h1> Dont be sorry father be better</h1>');
+});
+ 
+app.listen(3000);
