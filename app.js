c7e0a958684a29d726b4f56baee0071b49bed618
FrancisHenriquez <francishenriquez07@gmail.com>
2023-01-25 11:28:01 -0400

    404 not found status added

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 
+const bodyParser = require('body-parser');
 const express = require('express');
 
 //*Custom imports
 const adminRoutes = require('./routes/admin');
 const adminShop = require('./routes/shop')
 
-const app = express( ({ extended: false} ));
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use( adminRoutes );
-
 app.use( adminShop );
+
+app.use(( req, res, use ) => {
+    res.status(404).send('<h1> Page not found </h1>');
+
+});
 
  
 app.listen(3000);
